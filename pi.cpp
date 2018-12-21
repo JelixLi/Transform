937e7e3859430499a1fac825db1b88c471a4e7a6
JelixLi <1512510237@qq.com>
2018-12-21 16:55:28 +0800

    3

--- pi.cpp
+++ pi.cpp
@@ -59,12 +59,12 @@ void gemm(Ptr<Float> A,Ptr<Float> B,Ptr<Float> C,Int m,Int n,Int k) {
 void Init(SharedArray<float> input,int m,int n) {
   for(int i=0;i<m;i++) {
     for(int j=0;j<n;j++) {
-      input[i*n+j] = i*n+j;
+      input[i*n+j] = float(i*n+j);
     }
   }
 }
 
-void cpu_gemm(SharedArray<float> *A,SharedArray<float> *B,SharedArray<float> *C,int m,int n,int k) {
+void cpu_gemm(SharedArray<float> A,SharedArray<float> B,SharedArray<float> C,int m,int n,int k) {
   for(int i=0;i<m;i++) {
     for(int j=0;j<n;j++) {
       float sum = 0;
@@ -77,7 +77,7 @@ void cpu_gemm(SharedArray<float> *A,SharedArray<float> *B,SharedArray<float> *C,
 }
 
 
-void check(SharedArray<float> *A,float *B,int m,int n) {
+void check(SharedArray<float> A,float *B,int m,int n) {
   for(int i=0;i<m;i++) {
     for(int j=0;j<n;j++) {
       if(int(A[i*n+j])!=int(B[i*n+j])) {
