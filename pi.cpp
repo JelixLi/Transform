20cc633eeaab48d0a05022bc50a1dc71b563b7ae
JelixLi <1512510237@qq.com>
2018-12-21 19:20:21 +0800

    j

--- pi.cpp
+++ pi.cpp
@@ -197,7 +197,6 @@ void check(float *A,float *B,int m,int n) {
   for(int i=0;i<m;i++) {
     for(int j=0;j<n;j++) {
       if(int(A[i*n+j])!=int(B[i*n+j])) {
-          printf("%f  %f\n",A[i*n+j],B[i*n+j]);
           printf("error\n");
           return;
       }
@@ -269,7 +268,7 @@ int main() {
     transformToGpuFormat(B,image,height,width,channels,kernel_size,pad,stride);
 
     auto K=compile(gemm);
-    K.setNumQPUs(12);
+    K.setNumQPUs(9);
 
     clock_t start=clock();
     K(&A,&B,&C,m,n,k);
