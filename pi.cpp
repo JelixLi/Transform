0b8254e80ec87b35739836ef2d985c437f393a25
JelixLi <1512510237@qq.com>
2018-12-21 19:23:41 +0800

    j

--- pi.cpp
+++ pi.cpp
@@ -268,7 +268,7 @@ int main() {
     transformToGpuFormat(B,image,height,width,channels,kernel_size,pad,stride);
 
     auto K=compile(gemm);
-    K.setNumQPUs(9);
+    K.setNumQPUs(3);
 
     clock_t start=clock();
     K(&A,&B,&C,m,n,k);
