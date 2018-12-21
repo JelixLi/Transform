51d57207544b5924eeca341e940b4ded7612400a
JelixLi <1512510237@qq.com>
2018-12-21 17:04:57 +0800

    5

--- pi.cpp
+++ pi.cpp
@@ -56,7 +56,7 @@ void gemm(Ptr<Float> A,Ptr<Float> B,Ptr<Float> C,Int m,Int n,Int k) {
 #endif
 
 
-void Init(SharedArray<float> input,int m,int n) {
+void Init(SharedArray<float> &input,int m,int n) {
   for(int i=0;i<m;i++) {
     for(int j=0;j<n;j++) {
       input[i*n+j] = float(i*n+j);
@@ -64,7 +64,7 @@ void Init(SharedArray<float> input,int m,int n) {
   }
 }
 
-void cpu_gemm(SharedArray<float> A,SharedArray<float> B,float *C,int m,int n,int k) {
+void cpu_gemm(SharedArray<float> &A,SharedArray<float> &B,float *C,int m,int n,int k) {
   for(int i=0;i<m;i++) {
     for(int j=0;j<n;j++) {
       float sum = 0;
@@ -77,7 +77,7 @@ void cpu_gemm(SharedArray<float> A,SharedArray<float> B,float *C,int m,int n,int
 }
 
 
-void check(SharedArray<float> A,float *B,int m,int n) {
+void check(SharedArray<float> &A,float *B,int m,int n) {
   for(int i=0;i<m;i++) {
     for(int j=0;j<n;j++) {
       if(int(A[i*n+j])!=int(B[i*n+j])) {
