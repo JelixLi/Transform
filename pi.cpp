d12ee28e551cfe350862da2d4b7fde1cd355750d
JelixLi <1512510237@qq.com>
2018-12-22 01:10:03 +0800

    j

--- pi.cpp
+++ pi.cpp
@@ -547,7 +547,7 @@ float *get_image(int channels,int height,int width) {
 int main() {
     int output_num = 196;
 
-    int channels = 196;
+    int channels = 98;
     int height = 28;
     int width = 28;
     int pad = 2;
