a0c2c70f75979930fbb9559c5ab3aebb5aba0e8a
JelixLi <1512510237@qq.com>
2018-12-23 00:23:15 +0800

    j

--- manager.cpp
+++ manager.cpp
@@ -233,7 +233,7 @@ void GManager<T>::gpu_conv(
 
 template<typename T>
 void GManager<T>::Init_Gpu_Memory() {
-	_gp_array[0].alloc(22*(1<<20));
+	_gp_array[0].alloc(21*(1<<20));
 	_gp_array[1].alloc(21*(1<<20));
 	_gp_array[2].alloc(21*(1<<20));
 }
