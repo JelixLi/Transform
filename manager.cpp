0903a0d8dbb633db7e3c964de14da42fc96af149
JelixLi <1512510237@qq.com>
2019-01-27 23:06:06 +0800

    j

--- manager.cpp
+++ manager.cpp
@@ -256,7 +256,7 @@ void GManager<T>::gpu_conv(
         //   output_w,
         //   weight_group_size,
         //   input_group_size);
-        int num = i*weight_group_size*n+j*input_group_size;
+        int num = i*Gpu_Memory_Basic_Block*n+j*Gpu_Memory_Basic_Block;
         int r = num / n;
         int c = num % n;
         cout<<r<<" "<<c<<endl;
