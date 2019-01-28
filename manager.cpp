dba95ce53533df57f86fb1548c7f7dfbd836cb90
JelixLi <1512510237@qq.com>
2019-01-28 23:06:32 +0800

    j

--- manager.cpp
+++ manager.cpp
@@ -410,6 +410,47 @@ float *get_input(int height,int width,int channels) {
 }
 
 
+inline bool is_a_ge_zero_and_a_lt_b(int a, int b) {
+    return static_cast<unsigned>(a) < static_cast<unsigned>(b);
+}
+
+
+
+void im2col(const float *data_im, const int channels, const int height,
+            const int width, const int kernel_size,
+            const int pad, const int stride, float *data_col) {
+    const int output_h = (height + 2 * pad - kernel_size) / stride + 1;
+    const int output_w = (width + 2 * pad - kernel_size) / stride + 1;
+    const int channel_size = height * width;
+    for (int channel = channels; channel--; data_im += channel_size) {
+        for (int kernel_row = 0; kernel_row < kernel_size; kernel_row++) {
+            for (int kernel_col = 0; kernel_col < kernel_size; kernel_col++) {
+                int input_row = -pad + kernel_row;
+                for (int output_rows = output_h; output_rows; output_rows--) {
+                    if (!is_a_ge_zero_and_a_lt_b(input_row, height)) {
+                        for (int output_cols = output_w; output_cols; output_cols--) {
+                            *(data_col++) = 0;
+                        }
+                    } else {
+                        int input_col = -pad + kernel_col;
+                        for (int output_col = output_w; output_col; output_col--) {
+                            if (is_a_ge_zero_and_a_lt_b(input_col, width)) {
+                                *(data_col++) = data_im[input_row * width + input_col];
+                            } else {
+                                *(data_col++) = 0;
+                            }
+                            input_col += stride;
+                        }
+                    }
+                    input_row += stride;
+                }
+            }
+        }
+    }
+}
+
+
+
 int main() {
 
     auto GemmKernel = compile(gpu_gemm);
@@ -444,6 +485,9 @@ int main() {
     //   kernel_size,
     //   pad,
     //   stride);
+
+
+    // im2col(input,channels,height,width,kernel_size,pad,stride,col_data);
 
 
     gm.gpu_conv(
