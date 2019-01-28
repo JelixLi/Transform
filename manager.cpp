a19e39615562447865108872a4d8f9de726c47d6
JelixLi <1512510237@qq.com>
2019-01-28 23:15:21 +0800

    j

--- manager.cpp
+++ manager.cpp
@@ -422,20 +422,36 @@ void im2col(const float *data_im, const int channels, const int height,
     const int output_h = (height + 2 * pad - kernel_size) / stride + 1;
     const int output_w = (width + 2 * pad - kernel_size) / stride + 1;
     const int channel_size = height * width;
+
+    register float *data_col_ptr = data_col;
+    register float *data_im_ptr = data_im;
+
     for (int channel = channels; channel--; data_im += channel_size) {
         for (int kernel_row = 0; kernel_row < kernel_size; kernel_row++) {
             for (int kernel_col = 0; kernel_col < kernel_size; kernel_col++) {
                 int input_row = -pad + kernel_row;
                 for (int output_rows = output_h; output_rows; output_rows--) {
                     if (!is_a_ge_zero_and_a_lt_b(input_row, height)) {
-                        for (int output_cols = output_w; output_cols; output_cols--) {
-                            *(data_col++) = 0;
+                        int n = output_w/4;
+                        int _n = output_w/4;
+                        // for (int output_cols = output_w; output_cols; output_cols--) {
+                        //     *(data_col++) = 0;
+                        // }
+                        for(int i=0;i<n;i+=4) {
+                           *(data_col++) = 0;
+                           *(data_col++) = 0;
+                           *(data_col++) = 0;
+                           *(data_col++) = 0;
+                        }
+                        for(int i=0;i<n;i++) {
+                           *(data_col++) = 0;
                         }
                     } else {
                         int input_col = -pad + kernel_col;
                         for (int output_col = output_w; output_col; output_col--) {
                             if (is_a_ge_zero_and_a_lt_b(input_col, width)) {
-                                *(data_col++) = data_im[input_row * width + input_col];
+                                // *(data_col++) = data_im[input_row * width + input_col];
+                                *(data_col++) = *(data_im + input_row * width + input_col);
                             } else {
                                 *(data_col++) = 0;
                             }
@@ -487,7 +503,7 @@ int main() {
     //   stride);
 
 
-    // im2col(input,channels,height,width,kernel_size,pad,stride,col_data);
+    im2col(input,channels,height,width,kernel_size,pad,stride,col_data);
 
 
     gm.gpu_conv(
